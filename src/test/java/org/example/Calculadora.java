2090ff5507f5d2c5b019067968eea0d9754a523f
aurelio <marcoaureliocbarreto2008@gmail.com>
2024-03-14 14:42:31 -0300

    primeiro commit

---
+++ src/test/java/org/example/Calculadora.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Calculadora {
+    double precoUnitario = 25.00;
+    int quantidadeAdiquirida = 2;
+
+    public double calcular() {
+        return precoUnitario * quantidadeAdiquirida;
+    }
+
+    public double desconto() {
+
+            return 49;
+    }
+}
---
+++ src/test/java/org/example/Exercicio40Test.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import lombok.var;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class Exercicio40Test {
+    @Test
+    public void deve_calcular_valor_para_venda() {
+        double totalEsperado = 50;
+        var calculadora = new Calculadora();
+
+        double total = calculadora.calcular();
+
+        Assertions.assertEquals(totalEsperado, total);
+    }
+
+    @Test
+    public void deve_calcular_valor_com_desconto_cliente() {
+        double totalEsperado = 49;
+        var calculadora = new Calculadora();
+
+        double descontoCliente = calculadora.desconto();
+
+        Assertions.assertEquals(totalEsperado, descontoCliente);
+    }
+}
