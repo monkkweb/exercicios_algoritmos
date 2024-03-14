ecd487633239c6d606d6ab51a9ce2cd61186a06f
aurelio <marcoaureliocbarreto2008@gmail.com>
2024-03-14 16:23:33 -0300

    Correção doste que verifica desconto de 2

--- src/test/java/org/example/CalculadoraDeValorParaVenda.java
+++ src/test/java/org/example/CalculadoraDeValorParaVenda.java
@@ -3,13 +3,11 @@ package org.example;
 import lombok.var;
 
 public class CalculadoraDeValorParaVenda {
-    double precoUnitario = 25.00;
-    int quantidadeAdiquirida = 2;
 
-    public ValorParaVenda calcular() {
+    public ValorParaVenda calcular(double precoUnitario, int quantidadeAdiquirida) {
         var valorParaVenda = new ValorParaVenda();
         valorParaVenda.total = precoUnitario * quantidadeAdiquirida;
-        valorParaVenda.desconto = 49;
+        valorParaVenda.desconto = valorParaVenda.total * (1 - 0.02);
         return valorParaVenda;
     }
 
--- src/test/java/org/example/Exercicio40Test.java
+++ src/test/java/org/example/Exercicio40Test.java
@@ -5,22 +5,27 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class Exercicio40Test {
+
     @Test
     public void deve_calcular_valor_para_venda() {
         double totalEsperado = 50;
         var calculadora = new CalculadoraDeValorParaVenda();
+        int quantidade = 2;
+        double preco = 25;
 
-        var valorParaVenda = calculadora.calcular();
+        var valorParaVenda = calculadora.calcular(preco, quantidade);
 
         Assertions.assertEquals(totalEsperado, valorParaVenda.total);
     }
 
     @Test
-    public void deve_calcular_valor_com_desconto_cliente() {
-        double totalEsperado = 49;
+    public void se_quantidade_for_menor_ou_igual_cinco_aplica_desconto_de_dois_porcento() {
+        double totalEsperado = 98;
         var calculadora = new CalculadoraDeValorParaVenda();
+        var quantidade = 2;
+        double preco = 50;
 
-        var valorParaVenda = calculadora.calcular();
+        var valorParaVenda = calculadora.calcular(preco, quantidade);
 
         Assertions.assertEquals(totalEsperado, valorParaVenda.desconto);
     }
