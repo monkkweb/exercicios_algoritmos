1fc9c1216ad089cf92d6e70495730ee403e321a3
aurelio <marcoaureliocbarreto2008@gmail.com>
2024-03-15 17:41:28 -0300

    agrupamos os *descontos* e o *totalAhPagarEsperado* e adicionamos anotaÃção @parameterizedTest e @CsvSource

--- src/test/java/org/example/Exercicio40Test.java
+++ src/test/java/org/example/Exercicio40Test.java
@@ -1,16 +1,25 @@
 package org.example;
 
 import lombok.var;
-import org.junit.Before;
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class Exercicio40Test {
 
+    @InjectMocks
     private CalculadoraDeValorParaVenda calculadora;
 
-    @Before
-    public void init() {
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
         calculadora = new CalculadoraDeValorParaVenda();
     }
 
@@ -25,67 +34,21 @@ public class Exercicio40Test {
         Assertions.assertEquals(totalEsperado, valorParaVenda.total);
     }
 
-    @Test
-    public void se_quantidade_for_menor_ou_igual_cinco_aplica_desconto_de_dois_porcento() {
-        var descontoEsperado = 2;
-        var quantidade = 2;
-        var preco = 50;
-
+    @ParameterizedTest(name = "desconto é R${0} quando quantidade for {3}")
+    @CsvSource({"2,2,50,<= 5", "3,10,10,<= 10", "5,20,5,> 10"})
+    public void se_quantidade_for_menor_ou_igual_cinco_aplica_desconto_de_dois_porcento(double descontoEsperado,
+                                                                                        int quantidade, double preco,
+                                                                                        String operador) {
         var valorParaVenda = calculadora.calcular(preco, quantidade);
 
         Assertions.assertEquals(descontoEsperado, valorParaVenda.desconto);
     }
 
-    @Test
-    public void se_quantidade_for_maior_que_cinco_e_quantidade_menor_ou_igual_a_dez_aplica_desconto_tres_porcento() {
-        var descontoEsperado = 3;
-        var preco = 10;
-        var quantidade = 10;
-
-        ValorParaVenda valorParaVenda = calculadora.calcular(preco, quantidade);
-
-        Assertions.assertEquals(descontoEsperado, valorParaVenda.desconto);
-    }
-
-    @Test
-    public void se_quantidade_for_maior_que_dez_aplica_desconte_de_cinco_porcento() {
-        var descontoEsperado = 5;
-        var preco = 5;
-        var quantidade = 20;
-
-        var valorParaVenda = calculadora.calcular(preco, quantidade);
-
-        Assertions.assertEquals(descontoEsperado, valorParaVenda.desconto);
-    }
-
-    @Test
-    public void quando_quantidade_menor_que_cinco_desconto_de_2_porcento_eh_aplicado() {
-        var totalAhPagarEsperado = 98;
-        var quantidade = 2;
-        var preco = 50;
-
-        var valorParaVenda = calculadora.calcular(preco, quantidade);
-
-        Assertions.assertEquals(totalAhPagarEsperado, valorParaVenda.totalAhPagar);
-    }
-
-    @Test
-    public void quando_quantidade_menor_que_dez_e_maior_que_cinco_eh_aplicado_desconto_de_3_porcento() {
-        var totalAhPagarEsperado = 97;
-        var preco = 10;
-        var quantidade = 10;
-
-        var valorParaVenda = calculadora.calcular(preco, quantidade);
-
-        Assertions.assertEquals(totalAhPagarEsperado, valorParaVenda.totalAhPagar);
-    }
-
-    @Test
-    public void quando_quantidade_for_maior_que_dez_aplica_desconto_de_5_porcento() {
-        var totalAhPagarEsperado = 95;
-        var preco = 5;
-        var quantidade = 20;
-
+    @ParameterizedTest(name = "total a ser pago é R${0} quando for {3}")
+    @CsvSource({"98,2,50,<= 5", "97,10,10,<= 10", "95,20,5,> 10"})
+    public void quando_quantidade_menor_que_cinco_desconto_de_2_porcento_eh_aplicado(int totalAhPagarEsperado,
+                                                                                     int quantidade, double preco,
+                                                                                     String operador) {
         var valorParaVenda = calculadora.calcular(preco, quantidade);
 
         Assertions.assertEquals(totalAhPagarEsperado, valorParaVenda.totalAhPagar);
