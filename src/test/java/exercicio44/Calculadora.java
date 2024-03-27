74a3ab499b61a55177512a196cde565fec711d7f
wallace301089 <115509492+wallace301089@users.noreply.github.com>
2024-03-26 23:59:15 -0300

    foi feito a classe teste enquanto valor for zero repita

--- src/test/java/exercicio44/Calculadora.java
+++ src/test/java/exercicio44/Calculadora.java
@@ -3,13 +3,22 @@ package exercicio44;
 import lombok.var;
 
 public class Calculadora {
+    private Leitor leitor;
     private Impressora impressora;
 
-    public Calculadora(Impressora impressora) {
+    public Calculadora(Impressora impressora, Leitor leitor) {
         this.impressora = impressora;
+        this.leitor = leitor;
     }
 
-    public void calcular(double primeiroValor, double segundoValor) {
+    public void calcular() {
+        var primeiroValor = leitor.ler();
+        double segundoValor;
+
+        do {
+            segundoValor = leitor.ler();
+        } while (segundoValor == 0);
+
         var resultado = primeiroValor / segundoValor;
         impressora.imprimir(resultado);
     }
--- src/test/java/exercicio44/Exercicio44Teste.java
+++ src/test/java/exercicio44/Exercicio44Teste.java
@@ -6,16 +6,35 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class Exercicio44Teste {
 
     @Test
     public void O_segundo_valor_tem_que_ser_diferente_de_zero() {
-        var impressora = Mockito.mock(Impressora.class);
-        Calculadora calculadora = new Calculadora(impressora);
+        var impressora = mock(Impressora.class);
+        var leitor = mock(Leitor.class);
+        Calculadora calculadora = new Calculadora(impressora, leitor);
         double valorEsperado = 2;
         ArgumentCaptor<Double> argumentcaptor = ArgumentCaptor.forClass(Double.class);
 
-        calculadora.calcular(4, 2);
+        calculadora.calcular();
+
+        Mockito.verify(impressora).imprimir(argumentcaptor.capture());
+        Assertions.assertEquals(valorEsperado, argumentcaptor.getValue());
+    }
+
+    @Test
+    public void enquanto_o_segundo_valor_for_zero_repita() {
+        var leitor = mock(Leitor.class);
+        when(leitor.ler()).thenReturn(5.0).thenReturn(0.0).thenReturn(5.0);
+        Impressora impressora = mock(Impressora.class);
+        Calculadora calculadora = new Calculadora(impressora, leitor);
+        ArgumentCaptor<Double> argumentcaptor = ArgumentCaptor.forClass(Double.class);
+        double valorEsperado = 1;
+
+        calculadora.calcular();
 
         Mockito.verify(impressora).imprimir(argumentcaptor.capture());
         Assertions.assertEquals(valorEsperado, argumentcaptor.getValue());
