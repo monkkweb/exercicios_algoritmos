df2903defcc73fc5f18b7394d6d61b264d04fb64
wallace301089 <115509492+wallace301089@users.noreply.github.com>
2024-04-19 18:17:02 -0300

    foi feito o exercicio 75

---
+++ src/test/java/exercicio75/CalculadoraDeSequencia.java
@@ -0,0 +1,22 @@
+package exercicio75;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalculadoraDeSequencia {
+
+    public List<Integer> calcular() {
+        List<List<Integer>> resultado = new ArrayList<>();
+
+        for (int numero = 1; numero <= 10; numero++) {
+            List<Integer> numeros = new ArrayList<>();
+            numeros.add(numero);
+
+            for (int sequencia = 1; sequencia <= 10; sequencia++) {
+                numeros.add(sequencia);
+            }
+            resultado.add(numeros);
+        }
+        return resultado.get(9);
+    }
+}
---
+++ src/test/java/exercicio75/Exercicio75.java
@@ -0,0 +1,19 @@
+package exercicio75;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercicio75 {
+
+    @Test
+    void deve_calcular_a_ultima_sequencias_numericas() {
+        List<Integer> sequenciaNumericaEsperada = Arrays.asList(10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        CalculadoraDeSequencia calculadoraDeSequencia = new CalculadoraDeSequencia();
+
+        Assertions.assertEquals(sequenciaNumericaEsperada, calculadoraDeSequencia.calcular());
+    }
+}
