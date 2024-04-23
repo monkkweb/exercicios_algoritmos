fe71c5850f9d228b522e0331a1517a4d0e212c32
wallace301089 <115509492+wallace301089@users.noreply.github.com>
2024-04-23 18:50:15 -0300

    foi inicializado o exercicio 76

--- src/test/java/exercicio76/Exercicio76Test.java
+++ src/test/java/exercicio76/Exercicio76Test.java
@@ -1,21 +1,29 @@
 package exercicio76;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 public class Exercicio76Test {
-//    @InjectMocks
-//    private Matriz matriz;
-
 
     @Test
-    void deve_emprimi_sessenta_colunas(){
-        Assertions.assertEquals();
+    void deve_imprimi_sessenta_colunas() {
+        Impressora impressora = mock(Impressora.class);
+        String quantidadeDeColunaEsperada = "";
+
+
+        Matriz matriz = new Matriz(impressora);
+        ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+
+        matriz.calcular();
+
+        Mockito.verify(impressora).imprimir(argumentCaptor.capture());
+        Assertions.assertEquals(quantidadeDeColunaEsperada, argumentCaptor.getValue());
     }
 }
--- src/test/java/exercicio76/Matriz.java
+++ src/test/java/exercicio76/Matriz.java
@@ -1,54 +1,26 @@
 package exercicio76;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Matriz {
+    private Impressora impressora;
 
-    public static final int LINHAS = 10;
-    public static final int COLUNAS = 60;
-
-    public List<String> criarMatriz() {
-        List<List<Integer>> resultado = new ArrayList<>();
-
-        for (int i = 1; i <= LINHAS; i++) {
-            List<Integer> linhas = new ArrayList<>();
-            linhas.add(i);
-
-            for (int sequencia = 1; sequencia <= 10; sequencia++) {
-                numeros.add(sequencia);
-            }
-            resultado.add(numeros);
-        }
-        System.out.println(resultado);
-        return resultado.get(9);
+    public Matriz(Impressora impressora){
+        this.impressora = impressora;
     }
 
+    public String calcular() {
+        StringBuilder resultado = new StringBuilder();
+        char caractere;
+        for (int linha = 1; linha <= 10; linha++) {
+            for (int coluna = 1; coluna <= 60; coluna++) {
+                if (linha == 1 || linha == 10 || coluna == 1 || coluna == 60) {
+                    caractere = '+';
+                } else {
+                    caractere = ' ';
+                }
+                impressora.imprimir(String.valueOf(caractere));
+            }
+            impressora.imprimir("\n");
+        }
+        return resultado.toString();
+    }
 }
-//    public static void main(String[] args) {
-//
-//
-//        final int LINHAS = 10;
-//        final int COLUNAS = 60;
-//        String[][] matriz = criarMatriz(LINHAS, COLUNAS);
-//        String[][] matrizComBordaPreenchida = preencherBordaComCaracteres(matriz, LINHAS, COLUNAS, "+");
-//        String[][] matrizComTextoPosicionado = posicionar(matrizComBordaPreenchida, "Olá", 2, 10);
-//        for (int i = 0; i < LINHAS; i++) {
-//            for (int j = 0; j < COLUNAS; j++) {
-//                System.out.print(matrizComTextoPosicionado[i][j] + " ");
-//            }
-//            System.out.println();
-//
-//        }
-//    }
-//
-//    private static String[][] criarMatriz(int linhas, int colunas) {
-//        String[][] matriz = new String[linhas][colunas];
-//        for (int i = 0; i < linhas; i++) {
-//            for (int j = 0; j < colunas; j++) {
-//                matriz[i][j] = " ";
-//            }
-//        }
-//        return impressora.imprimir(matriz);
-//    }
-
